cb05182e2c3e204906a54bc4ca8fbacf78b5d106
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-03 18:39:13 +0800

    added binary Tree

---
+++ Algo/binaryTree/lesson1.js
@@ -0,0 +1,24 @@
+var TreeNode = /** @class */ (function () {
+    function TreeNode(val, left, right) {
+        if (left === void 0) { left = null; }
+        if (right === void 0) { right = null; }
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    return TreeNode;
+}());
+function printTree(node) {
+    if (!node)
+        return;
+    console.log(node.val);
+    printTree(node.left);
+    printTree(node.right);
+}
+var root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+printTree(root);
+console.log(root);
---
+++ Algo/binaryTree/lesson1.ts
@@ -0,0 +1,28 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val: number, left: TreeNode | null = null, right: TreeNode | null = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+function printTree(node: TreeNode | null): void {
+    if (!node) return;
+
+    console.log(node.val);
+
+    printTree(node.left);
+    printTree(node.right);
+}
+
+
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+printTree(root);
+console.log(root);
