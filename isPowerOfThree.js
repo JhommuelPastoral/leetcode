2e6dccf6125dc95e72899476f9f325429c7e1250
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-05 18:56:50 +0800

    isPowerOfThree

---
+++ isPowerOfThree.js
@@ -0,0 +1,14 @@
+function isPowerOfThree(n) {
+    if (n <= 0)
+        return false;
+    for (var i = 0; i <= n; i++) {
+        if (Math.pow(3, i) === n)
+            return true;
+        if (Math.pow(3, i) > n)
+            return false;
+    }
+    return false;
+}
+;
+console.log(isPowerOfThree(27)); // true
+console.log(isPowerOfThree(0));
---
+++ isPowerOfThree.ts
@@ -0,0 +1,12 @@
+function isPowerOfThree(n: number): boolean {
+    if(n <= 0) return false;
+    for(let i = 0; i <= n; i++){
+        if(Math.pow(3,i) === n) return true;
+        if(Math.pow(3,i) > n) return false;
+    }
+
+    return false;
+};
+
+console.log(isPowerOfThree(27)); // true
+console.log(isPowerOfThree(0));
