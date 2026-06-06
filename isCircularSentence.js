74f1d5091ae711180bd40d7b15c04dc5824665ee
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-06 19:37:27 +0800

    isCircularSentence

---
+++ isCircularSentence.js
@@ -0,0 +1,18 @@
+function isCircularSentence(sentence) {
+    var arr = sentence.split(' ');
+    if (arr.length === 1) {
+        if (arr[0][0] === arr[0][arr[0].length - 1])
+            return true;
+        else
+            return false;
+    }
+    for (var i = 0; i < arr.length - 1; i++) {
+        if (arr[i][arr[i].length - 1] !== arr[i + 1][0])
+            return false;
+    }
+    return arr[arr.length - 1][arr[arr.length - 1].length - 1] === arr[0][0];
+}
+;
+console.log(isCircularSentence("leetcode exercises sound delightful"));
+console.log(isCircularSentence("eetcode"));
+console.log(isCircularSentence("Leetcode is cool"));
---
+++ isCircularSentence.ts
@@ -0,0 +1,15 @@
+function isCircularSentence(sentence: string): boolean {
+    const arr = sentence.split(' ');
+    if(arr.length === 1){
+        if(arr[0][0] === arr[0][arr[0].length -1]) return true;
+        else return false;
+    }
+    for(let i = 0; i < arr.length - 1; i++){
+        if(arr[i][arr[i].length - 1] !== arr[i+1][0]) return false;
+    }
+    return arr[arr.length-1][arr[arr.length-1].length -1] === arr[0][0];
+};
+
+console.log(isCircularSentence("leetcode exercises sound delightful"));
+console.log(isCircularSentence("eetcode"));
+console.log(isCircularSentence("Leetcode is cool"));
