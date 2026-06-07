01f24c25e41556c76c899cdbe52e3d45c85cab13
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-07 17:04:10 +0800

    isMonotonic

---
+++ isMonotonic.js
@@ -0,0 +1,30 @@
+function isMonotonic(nums) {
+    if (nums.length === 1)
+        return true;
+    var isIncreasing = false;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] < nums[i + 1]) {
+            isIncreasing = true;
+            break;
+        }
+        else if (nums[i] === nums[i + 1])
+            continue;
+        else
+            break;
+    }
+    for (var i = 0; i < nums.length - 1; i++) {
+        if (isIncreasing) {
+            if (nums[i] > nums[i + 1])
+                return false;
+        }
+        else {
+            if (nums[i + 1] > nums[i])
+                return false;
+        }
+    }
+    return true;
+}
+;
+console.log(isMonotonic([1, 2, 3]));
+console.log(isMonotonic([6, 5, 4]));
+console.log(isMonotonic([1, 3, 2]));
---
+++ isMonotonic.ts
@@ -0,0 +1,26 @@
+function isMonotonic(nums: number[]): boolean {
+    if(nums.length === 1) return true;
+    let isIncreasing = false;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] < nums[i+1]){
+            isIncreasing = true;
+            break;
+        }
+        else if (nums[i] === nums[i+1]) continue;
+        else break;
+    }
+
+    for(let i = 0; i < nums.length - 1; i++){
+        if(isIncreasing){
+            if(nums[i] > nums[i+1]) return false;
+        }
+        else{
+            if(nums[i+1] > nums[i]) return false;
+        }
+    }
+    return true;
+};
+
+console.log(isMonotonic([1,2,3]));
+console.log(isMonotonic([6,5,4]));
+console.log(isMonotonic([1,3,2]));
