8aa9fe6a482ad5682193e199dd775c9745c30549
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-11 07:54:25 +0800

    isAdjacentDiffAtMostTwo

---
+++ isAdjacentDiffAtMostTwo.js
@@ -0,0 +1,11 @@
+function isAdjacentDiffAtMostTwo(s) {
+    for (var i = 0; i < s.length - 1; i++) {
+        if (Math.abs(Number.parseInt(s[i]) - Number.parseInt(s[i + 1])) > 2)
+            return false;
+    }
+    return true;
+}
+;
+console.log(isAdjacentDiffAtMostTwo("1234")); // true
+console.log(isAdjacentDiffAtMostTwo("1235")); // false
+console.log(isAdjacentDiffAtMostTwo("9876543210")); // true
---
+++ isAdjacentDiffAtMostTwo.ts
@@ -0,0 +1,13 @@
+function isAdjacentDiffAtMostTwo(s: string): boolean {
+    
+    for(let i = 0; i < s.length - 1; i++){
+        if(Math.abs(Number.parseInt(s[i]) - Number.parseInt(s[i+1])) > 2 ) return false;
+    }
+
+    return true;
+};
+
+
+console.log(isAdjacentDiffAtMostTwo("1234")); // true
+console.log(isAdjacentDiffAtMostTwo("1235")); // false
+console.log(isAdjacentDiffAtMostTwo("9876543210")); // true
