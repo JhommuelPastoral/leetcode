012cb6a505293b8099e44bcc9ee9f814598f0123
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-11 07:39:19 +0800

    findPeaks

---
+++ findPeaks.ts
@@ -0,0 +1,13 @@
+function findPeaks(mountain: number[]): number[] {
+    let res = [];
+    
+    for(let i = 1; i < mountain.length - 1; i++){
+        if(mountain[i-1] < mountain[i] && mountain[i] > mountain[i+1]) res.push(i);
+    }
+
+    return res;
+};
+
+console.log(findPeaks([0,1,0])); // [1]
+console.log(findPeaks([0,2,1,0])); // [1]
+console.log(findPeaks([0,1,2,3,4,5,6,7,8,9])); // []
