ce903097797e4750c3796a5092dc14888874772a
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-11 07:30:57 +0800

    validDigit

---
+++ validDigit.js
@@ -0,0 +1,9 @@
+function validDigit(n, x) {
+    var set = new Set(n.toString());
+    if (Number.parseInt(n.toString()[0]) === x)
+        return false;
+    return set.has(x.toString());
+}
+;
+console.log(validDigit(123, 1)); // true
+console.log(validDigit(123, 4)); // false
---
+++ validDigit.ts
@@ -0,0 +1,9 @@
+function validDigit(n: number, x: number): boolean {
+    const set = new Set<string>(n.toString());
+    if(Number.parseInt(n.toString()[0]) === x) return false;
+    return set.has(x.toString());
+};
+
+
+console.log(validDigit(123, 1)); // true
+console.log(validDigit(123, 4)); // false
