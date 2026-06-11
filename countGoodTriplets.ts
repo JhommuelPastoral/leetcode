4841f211d3ff14c482e877be69f334a55d5967cd
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-12 07:12:44 +0800

    countGoodTriplets

---
+++ countGoodTriplets.ts
@@ -0,0 +1,19 @@
+function countGoodTriplets(arr: number[], a: number, b: number, c: number): number {
+    let res = 0;
+    
+    for(let i = 0; i <= arr.length - 3; i++){
+        for(let j = i+1; j <= arr.length - 2; j++){
+            if(Math.abs(arr[i] - arr[j]) > a) continue;
+            for(let k = j+1; k < arr.length; k++){
+                if(Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c ) res++;
+            }
+        }
+    }
+
+
+    return res;
+};
+
+console.log(countGoodTriplets([3,0,1,1,9,7], 7, 2, 3));
+console.log(countGoodTriplets([1,1,2,2,3], 0, 0, 1));
+console.log(countGoodTriplets([7,3,4,5,6], 2, 3, 1));
