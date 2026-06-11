7a753892883b334c119cab99f622ede4b9a5a47c
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-11 08:20:43 +0800

    numTeams

---
+++ numTeams.js
@@ -0,0 +1,17 @@
+function numTeams(rating) {
+    var res = 0;
+    for (var i = 0; i <= rating.length - 3; i++) {
+        for (var j = i + 1; j <= rating.length - 2; j++) {
+            for (var k = j + 1; k < rating.length; k++) {
+                if (rating[i] < rating[j] && rating[j] < rating[k] ||
+                    rating[i] > rating[j] && rating[j] > rating[k])
+                    res++;
+            }
+        }
+    }
+    return res;
+}
+;
+console.log(numTeams([2, 5, 3, 4, 1]));
+console.log(numTeams([2, 1, 3]));
+console.log(numTeams([1, 2, 3, 4]));
---
+++ numTeams.ts
@@ -0,0 +1,20 @@
+function numTeams(rating: number[]): number {
+    let res = 0;
+
+    for(let i = 0; i <= rating.length - 3; i++){
+        for(let j = i +1; j <= rating.length - 2; j++){
+            for(let k = j +1; k < rating.length; k++){
+                if (rating[i] < rating[j] && rating[j] < rating[k] ||
+                    rating[i] > rating[j] && rating[j] > rating[k]) res++;       
+            }
+        }
+    }
+
+    return res;
+
+};
+
+
+console.log(numTeams([2,5,3,4,1]));
+console.log(numTeams([2,1,3]));
+console.log(numTeams([1,2,3,4]));
