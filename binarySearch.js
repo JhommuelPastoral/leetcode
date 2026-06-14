6b0028e43110e5c0b4460e85deaa13becff6fb26
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-15 05:18:02 +0800

    binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,15 @@
+function binarySearch(arr, target) {
+    var left = 0, right = arr.length - 1;
+    while (left <= right) {
+        var mid = left + Math.floor((right - left) / 2);
+        if (arr[mid] === target)
+            return mid;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+console.log(binarySearch([1, 2, 3, 4, 5], 3));
+console.log(binarySearch([1, 2, 3, 4, 5], 6));
