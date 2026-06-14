f40ff1672193515002135b8e4acac3f2b230f20b
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-15 07:32:34 +0800

    threeSum1

---
+++ threeSum1.js
@@ -0,0 +1,29 @@
+function threeSum(nums) {
+    var res = [];
+    var seen = new Set();
+    var sorted = nums.sort(function (a, b) { return a - b; });
+    var length = nums.length - 1;
+    for (var i = 0; i < nums.length; i++) {
+        var first = nums[i];
+        var left = i + 1;
+        var right = length;
+        while (left < right) {
+            if (first + nums[left] + nums[right] > 0)
+                right--;
+            else if (first + nums[left] + nums[right] < 0)
+                left++;
+            else if (first + nums[left] + nums[right] === 0) {
+                var str = "".concat(first, ",").concat(nums[left], ",").concat(nums[right]);
+                if (!seen.has(str)) {
+                    seen.add(str);
+                    res.push([first, nums[left], nums[right]]);
+                }
+                right--;
+            }
+        }
+    }
+    return res;
+}
+console.log(threeSum([-1, 0, 1, 2, -1, -4])); // Output: [[-1,-1,2],[-1,0,1]]
+console.log(threeSum([0, 1, 1])); // Output: []
+console.log(threeSum([0, 0, 0])); // Output: [[0,0,0]]
---
+++ threeSum1.ts
@@ -0,0 +1,30 @@
+function threeSum(nums: number[]): number[][] {
+    const res: number[][] = [];
+    const seen = new Set<string>();
+    const sorted = nums.sort((a,b) => a - b);
+    const length = nums.length - 1;
+    for (let i = 0; i < nums.length; i++) {
+        const first = nums[i];
+        let left = i + 1;
+        let right = length;
+        while(left < right){
+            if(first + nums[left] + nums[right] > 0) right--;
+            else if (first + nums[left] + nums[right] < 0) left++;
+            else if (first + nums[left] + nums[right] === 0){
+                const str = `${first},${nums[left]},${nums[right]}`;
+                if(!seen.has(str)){
+                    seen.add(str);
+                    res.push([first, nums[left], nums[right]])
+                }
+                right--;
+            }
+        }
+
+    }
+
+    return res;
+}
+
+console.log(threeSum([-1,0,1,2,-1,-4])); // Output: [[-1,-1,2],[-1,0,1]]
+console.log(threeSum([0,1,1])); // Output: []
+console.log(threeSum([0,0,0])); // Output: [[0,0,0]]
