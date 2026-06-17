1c0de95474768b05dc9a9b02c8e1396498aac79f
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-17 17:42:39 +0800

    findMaxK

---
+++ findMaxK.js
@@ -0,0 +1,23 @@
+function findMaxK(nums) {
+    nums.sort(function (a, b) { return a - b; });
+    var left = 0;
+    var right = nums.length - 1;
+    var max = -1;
+    while (left < right) {
+        if (nums[left] > 0 || nums[right] < 0)
+            break;
+        if (nums[left] * -1 > nums[right])
+            left++;
+        else if (nums[left] * -1 < nums[right])
+            right--;
+        else {
+            max = Math.max(max, nums[right]);
+            left++;
+            right--;
+        }
+    }
+    return max;
+}
+;
+console.log(findMaxK([-1, 2, -3, 3])); // Output: 3
+console.log(findMaxK([-1, 10, 6, 7, -7, 1])); // Output: 7
---
+++ findMaxK.ts
@@ -0,0 +1,21 @@
+function findMaxK(nums: number[]): number {
+    nums.sort((a:number, b:number) => a - b);
+    let left = 0;
+    let right = nums.length - 1;
+    let max = -1;
+    while(left < right){
+        if(nums[left] > 0 || nums[right] < 0) break; 
+        if(nums[left] * -1 > nums[right]) left++;
+        else if(nums[left] * -1 < nums[right]) right--;
+        else {
+            max = Math.max(max, nums[right]);
+            left++;
+            right--;
+        }
+    }
+    return max;
+};
+
+
+console.log(findMaxK([-1, 2, -3, 3])); // Output: 3
+console.log(findMaxK([-1, 10, 6, 7, -7, 1])); // Output: 7
