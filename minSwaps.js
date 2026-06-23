8700a4994a57a8cc74c328ddd6ffca4e0a360ae1
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-06-23 18:35:47 +0800

    minSwaps Optimized

--- minSwaps.js
+++ minSwaps.js
@@ -1,33 +1,33 @@
 function minSwaps(nums) {
+    var _a;
     var res = 0;
     var map = new Map();
-    for (var i = 0; i < nums.length; i++) {
+    for (var _i = 0, nums_1 = nums; _i < nums_1.length; _i++) {
+        var num = nums_1[_i];
         var sum = 0;
-        var num = nums[i];
-        while (num > 0) {
-            sum += num % 10;
-            num = Math.floor(num / 10);
+        var temp = num;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp = Math.floor(temp / 10);
         }
-        map.set(nums[i], sum);
+        map.set(num, sum);
     }
     var sorted = Array.from(map.entries()).sort(function (a, b) { return a[1] - b[1] || a[0] - b[0]; });
+    var pos = new Map();
     for (var i = 0; i < nums.length; i++) {
-        if (nums[i] !== sorted[i][0]) {
-            for (var j = 0; j < nums.length; j++) {
-                if (i === j)
-                    continue;
-                if (nums[j] === sorted[i][0]) {
-                    var temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    res++;
-                }
-            }
-        }
+        pos.set(nums[i], i);
     }
-    console.log(sorted);
+    for (var i = 0; i < nums.length; i++) {
+        var target = sorted[i][0];
+        if (nums[i] === target)
+            continue;
+        var j = pos.get(target);
+        _a = [nums[j], nums[i]], nums[i] = _a[0], nums[j] = _a[1];
+        pos.set(nums[j], j);
+        pos.set(nums[i], i);
+        res++;
+    }
     return res;
 }
-;
 console.log(minSwaps([1, 10, 2, 9, 3, 8, 4, 7, 5, 6]));
 console.log(minSwaps([1, 3, 5, 2, 4, 6, 7]));
--- minSwaps.ts
+++ minSwaps.ts
@@ -1,34 +1,43 @@
 function minSwaps(nums: number[]): number {
     let res = 0;
-    const map = new Map<number,number>();
-    for(let i = 0; i < nums.length; i++){
+
+    const map = new Map<number, number>();
+
+    for (const num of nums) {
         let sum = 0;
-        let num = nums[i];
-        while(num > 0){
-            sum += num % 10;
-            num = Math.floor(num / 10);
-        }
-        map.set(nums[i], sum);
-    }
-    const sorted = Array.from(map.entries()).sort((a:number[], b:number[]) => a[1] - b[1] || a[0] - b[0]);
+        let temp = num;
 
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] !== sorted[i][0]){
-            for(let j = 0; j < nums.length; j++){
-                if(i === j) continue;
-                if(nums[j] === sorted[i][0]){
-                    const temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    res++;
-                }
-            }
+        while (temp > 0) {
+            sum += temp % 10;
+            temp = Math.floor(temp / 10);
         }
+
+        map.set(num, sum);
     }
-    console.log(sorted)
+
+    const sorted = Array.from(map.entries()).sort(
+        (a, b) => a[1] - b[1] || a[0] - b[0]
+    );
+
+    const pos = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        pos.set(nums[i], i);
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        const target = sorted[i][0];
+
+        if (nums[i] === target) continue;
+        const j = pos.get(target)!;
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+        pos.set(nums[j], j);
+        pos.set(nums[i], i);
+        res++;
+    }
+
     return res;
-
-};
+}
 
 console.log(minSwaps([1,10,2,9,3,8,4,7,5,6]));
 console.log(minSwaps([1,3,5,2,4,6,7]));
