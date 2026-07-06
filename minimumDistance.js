dba0e0c700e40f64563f3fe50ded5446b3d711b8
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2026-07-06 09:30:16 +0800

    minimumDistance

---
+++ minimumDistance.js
@@ -0,0 +1,19 @@
+function minimumDistance(nums) {
+    var min = Infinity;
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = 0; j < nums.length; j++) {
+            if (i === j || nums[i] !== nums[j])
+                continue;
+            for (var k = 0; k < nums.length; k++) {
+                if (i === k || j === k || nums[j] !== nums[k])
+                    continue;
+                if (nums[j] === nums[k])
+                    min = Math.min(min, Math.abs(i - j) + Math.abs(k - j) + Math.abs(k - i));
+            }
+        }
+    }
+    return min === Infinity ? -1 : min;
+}
+;
+console.log(minimumDistance([1, 2, 3, 1, 1, 3])); // 8
+console.log(minimumDistance([1, 2, 3, 4, 5])); // -1
